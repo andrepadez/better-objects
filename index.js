60424ab9f563bded0afd7fa9c4f84cb9bf5ebe5a
André Alçada Padez <andre.padez@gmail.com>
2014-12-16 16:41:30 +0100

    cloning EVERYTHING

--- index.js
+++ index.js
@@ -1,99 +1,114 @@
-Object.prototype.clone = function() {
-    var theClone;
+(function(window, undefined){
 
-    var traverseObject = function(obj, clone){
-        Object.keys(obj).forEach(function(key){
-            if(obj[key] === null){
-                clone[key] = null;
+    if(typeof Object.prototype.clone !== 'function'){
+        Object.prototype.clone = function() {
+            var theClone; 
+
+            if( Array.isArray(this) ){
+                theClone = traverseArray(this);
+            } else {
+                theClone = {};
+                traverseObject(this, theClone);    
             }
-            if(Array.isArray(obj[key])){
-                clone[key] = traverseArray(obj[key]);
+            
+            return theClone;
+        };
+
+        var traverseObject = function(obj, clone){
+            Object.keys(obj).forEach(function(key){
+                clone[key] = cloneProperty(obj[key]);
+            });
+        };
+
+        var traverseArray = function(arr){
+            var finalArray = [];
+            arr.forEach(function(item){
+                finalArray.push(cloneProperty(item));
+            });
+            return finalArray;
+        };
+
+        var cloneProperty = function(prop){
+            var clone;
+            if(prop === null){
+                clone = null;
+            }
+            if(Array.isArray(prop)){
+                clone = traverseArray(prop);
             } 
-            else if(obj[key].constructor === Date){
-                clone[key] = new Date(obj[key].toString());
+            else if(prop.constructor === Date){
+                clone = new Date(prop.toString());
             }
-            //http://www.bennadel.com/blog/2664-cloning-regexp-regular-expression-objects-in-javascript.htm
-            else if(obj[key].constructor === RegExp){
-                clone[key] = new Date(obj[key].toString());
+            else if(prop.constructor === RegExp){
+                clone = new cloneRegExp(prop);
             }
-            else if(typeof obj[key] === 'object'){
-                clone[key] = {};
-                traverseObject(obj[key], clone[key]);
+            else if(typeof prop === 'object'){
+                clone = {};
+                traverseObject(prop, clone);
             } 
             //we don't clone functions for performance reasons
-            // else if(typeof obj[key] === 'function'){
-            //     clone[key] = eval(obj[key].toString());
+            // else if(typeof prop === 'function'){
+            //     clone = eval(prop.toString());
             // }
             else {
-                clone[key] = obj[key];
+                clone = prop;
             }
-        });
-    };
-
-    var traverseArray = function(arr){
-        var finalArray = [];
-        arr.forEach(function(item){
-            if(Array.isArray(item)){
-                finalArray.push( traverseArray(item) );
+            return clone;
+        };
+        
+        //http://www.bennadel.com/blog/2664-cloning-regexp-regular-expression-objects-in-javascript.htm
+        var cloneRegExp = function(input, injectFlags){
+            var pattern = input.source;
+            var flags = '';
+            injectFlags = (injectFlags || '');
+            if (input.global || (/g/i).test(injectFlags)){
+                flags += "g";
             }
-            else if(typeof item === 'object'){
-                var newObj = {};
-                traverseObject(item, newObj)
-                finalArray.push( newObj );
-            } 
-            else {
-                finalArray.push(item);
+            if (input.ignoreCase || (/i/i).test(injectFlags)){
+                flags += "i";
             }
-        });
-        return finalArray;
-    }; 
-
-    if( Array.isArray(this) ){
-        theClone = traverseArray(this);
-    } else {
-        theClone = {};
-        traverseObject(this, theClone);    
+            if (input.multiline || (/m/i).test(injectFlags)){
+                flags += "m";
+            }
+            return(new RegExp(pattern, flags ));
+        }
     }
-    
-    return theClone;
-};
 
-Object.prototype.equals = function(target){
-    var equals = true;
-    (function traverse(one, two){
-        if(typeof one === 'function'){
-            if(one.toString() !== two.toString()){
-                equals = false;
-            }
-        }
-        else if([Date, RegExp].indexOf(one.constructor) > -1){console.log(one.toString(), two.toString(), one.toString() === two.toString())
-            if(one.toString() !== two.toString()){
-                equals = false;
-            }
-        }
-        else if( Array.isArray(one) ){
-            if(one.length !== two.length){
-                equals = false;
-            }
-            one.forEach(function(val, idx){
-                traverse( one[idx], two[idx] );
-            });
-        }
-        else if( typeof one === 'object' ){
-            if(Object.keys(one).length !== Object.keys(two).length){
-                equals = false;
-            }
-            Object.keys(one).forEach(function(key){
-                traverse(one[key], two[key]);
-            });
-        } 
-        //primitives
-        else {
-            if(one !== two){
-                equals = false;
-            }
-        }
-    })(this, target);
+    if(typeof Object.prototype.equals !== 'function'){
+        Object.prototype.equals = function(target){
+            var equals = true;
+            (function traverse(one, two){
+                if([Date, RegExp].indexOf(one.constructor) > -1 || typeof one === 'function'){
+                    if(one.toString() !== two.toString()){
+                        equals = false;
+                    }
+                }
+                else if( Array.isArray(one) ){
+                    if(one.length !== two.length){
+                        equals = false;
+                    }
+                    one.forEach(function(val, idx){
+                        traverse( one[idx], two[idx] );
+                    });
+                }
+                else if( typeof one === 'object' ){
+                    if(Object.keys(one).length !== Object.keys(two).length){
+                        equals = false;
+                    }
+                    Object.keys(one).forEach(function(key){
+                        traverse(one[key], two[key]);
+                    });
+                } 
+                //primitives
+                else {
+                    if(one !== two){
+                        equals = false;
+                    }
+                }
+            })(this, target);
 
-    return equals;
-};
+            return equals;
+        };
+    }
+
+})(window);
